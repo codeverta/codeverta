9059ed93fdf4a2a3886d5435fa5c0c961e1d38d4
rabihutomo <rabihutomo11@gmail.com>
2025-08-05 10:53:25 +0700

    feat: image domain

--- next.config.js
+++ next.config.js
@@ -14,15 +14,12 @@ const nextConfig = {
   async headers() {
     return [
       {
+        // Applies to all routes
         source: '/:path*',
         headers: [
           {
-            key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin',
-          },
-          {
-            key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp',
+            key: 'Cross-Origin-Resource-Policy',
+            value: 'cross-origin', // or 'same-site'
           },
         ],
       },
