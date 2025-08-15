2668ab542f1a54e9ff3fab2b17f72010ba109bc0
rabihutomo <rabihutomo11@gmail.com>
2025-08-15 07:58:52 +0700

    improve sitemap

--- components/CodeBlock.tsx
+++ components/CodeBlock.tsx
@@ -1,5 +1,13 @@
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
-import { vscDarkPlus } from "react-syntax-highlighter/dist/esm/styles/prism";
+import {
+  atomDark,
+  tomorrow,
+  prism,
+  twilight,
+  solarizedlight,
+  dracula,
+  vscDarkPlus,
+} from "react-syntax-highlighter/dist/cjs/styles/prism";
 
 interface CodeBlockProps {
   language: string;
