bc40c868e1cf7d5dc6b3e3af1dea6b3a345e8045
rabihutomo <rabihutomo11@gmail.com>
2025-09-14 08:36:30 +0700

    update

--- pages/tutorials/[id].tsx
+++ pages/tutorials/[id].tsx
@@ -3,7 +3,7 @@ import React from "react";
 import Head from "next/head";
 import Link from "next/link";
 import { NextSeo } from "next-seo";
-import { getPostData, getAllPostIds } from "lib/news";
+import { getPostData, getAllPostIds } from "lib/posts";
 import {
   Calendar,
   User,
